29680328ed4fbe40cb6f18e19b070bf87e93a927
Vianey Vargas <vianeyjava@gmail.com>
2024-11-23 23:26:30 -0500

    Datos recibidos desde Meta en el archivo apiController funcion recibir y veriricar

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,9 +1,44 @@
 const verificar = (req, resp) => {
-    resp.send('Verificado!')
+    try {
+        var tokenMeta = "chatbootwoquimeta";
+        var token = req.query["hub.verify_token"];
+        var challenge = req.query["hub.challenge"];
+
+        if (challenge != null && token != null && token == tokenMeta) {
+            resp.send(challenge);
+        } else {
+            resp.status(400).send();
+
+        }
+        console.log(req);
+
+    } catch (error) {
+        //Le enviamos el status 400 para que Meta no este intentando verificar constantemente
+        resp.status(400).send();
+    }
 }
 
 const recibir = (req, resp) => {
-    resp.send('Recibido!')
+    try {
+        var entry = (req.body["entry"])[0];
+        var changes = (entry["changes"])[0];
+        var value = changes["value"];
+        var objetoMensaje = value["messages"];
+
+        var message = objetoMensaje[0];
+
+        var texto = message["text"]["body"];
+        var number = message["form"];
+
+        console.log("Enviado desde: " + number + " El texto es el siguiente: " + texto);
+        // console.log(objetoMensaje);
+        resp.send('EVENT_RECEIVED');
+    } catch (error) {
+        console.log(error);
+        resp.send('EVENT_RECEIVED');
+    }
+    console.log(req);
+
 }
 
 module.exports = {
