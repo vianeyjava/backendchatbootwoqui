7d2e712dd46a446a3fe54a60ef9874a54e33b20c
Vianey Vargas <vianeyjava@gmail.com>
2024-11-25 14:58:29 -0500

    Se modifica lambda.js

--- src/lambda.js
+++ src/lambda.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = require('./app'); // Importa tu app existente
-
-module.exports.handler = serverless(app); // Exporta el manejador para AWS Lambda
+export const handler = async(event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello, World!" }),
+    };
+};
