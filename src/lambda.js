18405182d43abed33e2b30bdd686978fd641612f
Vianey Vargas <vianeyjava@gmail.com>
2024-11-25 15:41:19 -0500

    Se modifica el archivo lambda.js

--- src/lambda.js
+++ src/lambda.js
@@ -1,6 +1,11 @@
-export const handler = async(event) => {
+exports.handler = async(event) => {
     return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello, World!" }),
+        statusCode: 200, // Devuelve 200 para confirmar que funciona
+        body: JSON.stringify({
+            message: "Hello, World!",
+            method: event.httpMethod, // Muestra el método HTTP usado (GET, POST, etc.)
+            headers: event.headers, // Incluye los encabezados de la solicitud
+            queryString: event.queryStringParameters // Incluye los parámetros de consulta
+        }),
     };
 };
